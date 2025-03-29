a1e2111d684277e31211ffca7a4fbc37342df611
abelsaberhagen <abelsaberhagen@gmail.com>
2025-03-29 18:55:01 -0400

    added some things
    some things

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const submitButton = document.querySelector(".sendButton")
 submitButton.addEventListener("click" , () => {
     sendMessage()
 })
-function sendMessage() {
-    let input = document.getElementById("user-input");
-    let message = input.value.trim();
-    if (message === "") return;
 
-    let chatBox = document.getElementById("chat-box");
-    let userMessage = `<p><strong>You:</strong> ${message}</p>`;
+async function sendMessage() {
+    const input = document.getElementById("audio-upload");
+    const chatBox = document.getElementById("chat-box");
+    const audio_file = input.files[0];
+    const formData = new FormData()
+    formData.append(audio_file)
+
+    const image_json = await fetch("http://127.0.0.1:5500/", {
+        method: "POST",
+        body: JSON.stringify({ File: input.file[0] }),
+    });
+    
+    
+    let userMessage = `<p><strong>You:</strong> ${audio_file}</p>`;
     chatBox.innerHTML += userMessage;
     input.value = "";
     
